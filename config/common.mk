df1b4ebb3853059ca537d37a2f7d82f80ae55519
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    vendor: Use Pixel charger animation
    Change-Id: I89ac5aa6c4dec0ded4524e94dc07fd268690b5f4
    Signed-off-by: Arseniy Graur <argraur@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -98,6 +98,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/pixelage/prebuilt/common/etc/init/init.pixelage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.pixelage-updater.rc
 
+# Charger
+PRODUCT_PACKAGES += \
+    product_charger_res_images
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleSettingsConfig
