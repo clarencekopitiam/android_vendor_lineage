ea4d3e4034836c0def8b52dfd24940c226023a0d
clarencelol <clarencekuiek@proton.me>
2023-10-16 22:58:04 +0530

    vendor: Remove unused packages

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -3,17 +3,12 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
 # Apps
 PRODUCT_PACKAGES += \
     Eleven \
     Etar \
     Profiles \
-    Recorder \
-    Seedvault
+    Recorder
 
 ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
@@ -29,11 +24,6 @@ endif
 PRODUCT_PACKAGES += \
     unrar \
     zstd
-
-# Fonts
-PRODUCT_PACKAGES += \
-    FontLatoOverlay \
-    FontRubikOverlay
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
